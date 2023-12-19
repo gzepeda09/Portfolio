63a16743abc55ac68d9d24206780094152de21b7
gzepeda09 <gzepeda09@gmail.com>
2023-12-18 21:58:55 -0800

    Fized index html

--- vite.config.js
+++ vite.config.js
@@ -1,23 +1,10 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   base: '/portfolio/',
   outDir: 'dist',
-  esbuild: {
-     jsxFactory: 'h',
-     jsxFragment: 'Fragment',
-   },
-
-  build: {
-      // other build options...
-      rollupOptions: {
-        output: {
-          // Set the script type for JSX files
-          manualChunks: { 'main.jsx': ['jsx'] },
-        },
-      },
-    },
-})
+  // Remove esbuild configuration
+  // Remove unnecessary rollupOptions
+});
