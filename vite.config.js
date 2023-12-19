e031a0ae168303bcf632b793326df1826a3f3e2f
gzepeda09 <gzepeda09@gmail.com>
2023-12-18 21:53:53 -0800

    updated vite.confing.js

--- vite.config.js
+++ vite.config.js
@@ -6,4 +6,18 @@ export default defineConfig({
   plugins: [react()],
   base: '/portfolio/',
   outDir: 'dist',
+  esbuild: {
+     jsxFactory: 'h',
+     jsxFragment: 'Fragment',
+   },
+
+  build: {
+      // other build options...
+      rollupOptions: {
+        output: {
+          // Set the script type for JSX files
+          manualChunks: { 'main.jsx': ['jsx'] },
+        },
+      },
+    },
 })
