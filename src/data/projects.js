cdd0ca26e4893b3f4961cec0771aa2e4d5c9f61b
gzepeda09 <gzepeda09@gmail.com>
2024-01-13 00:30:27 -0800

    please work pics

--- src/data/projects.js
+++ src/data/projects.js
@@ -11,7 +11,7 @@ export default [
 		"name":"RowdyHealth",
 		"key" : "1",
 		"stack": ["MySQL", "HTML/CSS", "PHP", "JavaScript"],
-		"imgUrl" : "./public/assets/rh.jpg",
+		"imgUrl" : "./assets/rh.jpg",
 		"link" : "https://artemis.cs.csub.edu/~ramrod/Team-Ramrod/",
 
 	},
@@ -20,7 +20,7 @@ export default [
 		"name":"JumpStarter",
 		"key" : "2",
 		"stack": ["MongoDB", "ExpressJS", "ReactJS", "NodeJS"],
-		"imgUrl" : "./public/assets/js.JPG",
+		"imgUrl" : "./assets/js.JPG",
 		"link" : "https://github.com/jumpstarter3390",
 
 	},
